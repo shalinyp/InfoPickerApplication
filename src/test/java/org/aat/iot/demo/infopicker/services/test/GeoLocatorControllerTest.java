e1aca968cced117e5dbb21e0ec871e671c2ac363
shalinyp <shalinyp@gmail.com>
2016-05-26 16:27:18 +0530

    Renamed method

--- src/test/java/org/aat/iot/demo/infopicker/services/test/GeoLocatorControllerTest.java
+++ src/test/java/org/aat/iot/demo/infopicker/services/test/GeoLocatorControllerTest.java
@@ -1,55 +1,51 @@
 package org.aat.iot.demo.infopicker.services.test;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.UnknownHostException;
-import java.nio.file.Files;
 
 import org.aat.iot.demo.infopicker.AppIotPlatformFactory;
+import org.aat.iot.demo.infopicker.model.EventData;
 import org.aat.iot.demo.infopicker.services.GeoLocatorController;
 import org.aat.iot.demo.infopicker.test.TestConstants;
-import org.apache.commons.compress.utils.IOUtils;
 import org.junit.Before;
 import static org.junit.Assert.*;
-import org.junit.Rule;
 import org.junit.Test;
 
 public class GeoLocatorControllerTest {
-  //Class under Test
+  // Class under Test
   AppIotPlatformFactory appIotFactory = new AppIotPlatformFactory();
   GeoLocatorController geoLocatorController;
 
-  
+
   @Before
-  public void setUp()
-  {   
+  public void setUp() {
     geoLocatorController = new GeoLocatorController();
   }
-  
-  @Test
-  public void testWhetherCorrectMapSizeIsReturnOnRightParameters() throws IOException{
 
-    FileInputStream output = new FileInputStream("src\\test\\resources\\staticmap.png");
-    byte[] imageBytes = IOUtils.toByteArray(output);
-    byte[] mapBytes = geoLocatorController.getMap(TestConstants.GOOGLE_MAP_URL,"Australia");
-    assertEquals(imageBytes.length,mapBytes.length);
-    
+  @Test
+  public void testWhetherValidMapIsReturnOnRightParameters() throws IOException {
+
+    // FileInputStream output = new FileInputStream("src\\test\\resources\\staticmap.png");
+    // byte[] imageBytes = IOUtils.toByteArray(output);
+    byte[] mapBytes = geoLocatorController.getMap(TestConstants.GOOGLE_MAP_URL, "Australia");
+    assertTrue(mapBytes.length > 0);
+
   }
-  
+
   @Test(expected = UnknownHostException.class)
-  public void testWhetherUnknownHostExceptionIsThrownOnInvalidUrl() throws UnknownHostException  {
-      try {
-        geoLocatorController.getMap("http://ms.testdummy.dummy/map/api/staticmap?center=","Australia");
-      } catch (IOException e) {
-          throw new UnknownHostException();
-      }
-    
+  public void testWhetherUnknownHostExceptionIsThrownOnInvalidUrl() throws UnknownHostException {
+    try {
+      geoLocatorController.getMap("http://ms.testdummy.dummy/map/api/staticmap?center=",
+          "Australia");
+    } catch (IOException e) {
+      throw new UnknownHostException();
+    }
+
+  }
+
+  @Test
+  public void testWhetherGetInformationReturnsEventData() {
+    assertTrue(geoLocatorController.getInformation() instanceof EventData);
   }
 
 }
